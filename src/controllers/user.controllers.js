6c85da3bb0df4d8dfc8e714d68f5a46741a084a5
roberto <roberto1608torales@gmail.com>
2023-12-05 23:32:22 -0300

    ok

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,197 @@
+import UserServices from "../services/user.services.js";
+import CustomeError from "../services/error/customeError.js";
+import EErrors from "../services/error/errorsEnum.js";
+
+class UserController{
+    constructor(){
+        this.UserServices = new UserServices();
+    }
+
+    async register(req, res, next) {
+        try {
+          const { first_name, last_name, email, age, password, role } = req.body;
+        if( !first_name || !email || !age || !password){
+          const customeError = new CustomeError({
+            name: "User creation error",
+            cause: generateUserError({
+              first_name, last_name, email, age, password,
+            }),
+            message: "Error al intentar registrar al usuario",
+            code:400,
+          });
+          return next(customeError);
+        }
+        const response = await this.userService.register({
+          first_name,
+          last_name,
+          email,
+          age,
+          password,
+          role,
+        });
+    
+        return res.status(response.status === "success" ? 200 : 400).json({
+          status: response.status,
+          data: response.user,
+          redirect: response.redirect,
+        });
+        } catch (error) {
+          return next(error);
+        }
+        
+      }
+
+      async restorePassword(req, res, next) {
+    
+        try {
+          const { user, pass } = req.query;
+          const passwordRestored = await this.userService.restorePassword(user, createHash(pass));
+          if (passwordRestored) {
+            return res.send({status: "OK", message: "La contraseña se ha actualizado correctamente"});
+          } else {
+            const customeError = new CustomeError({
+              name: "Restore Error",
+              massage: "No fue posible actualizar la contraseña",
+              code: EErrors.PASSWORD_RESTORATION_ERROR,
+            });
+            return next(customeError);  
+          }
+        } catch (error) {
+          req.logger.error(error);
+          return next(error);
+        }
+      }
+    
+
+      current(req, res, next) {
+        if (req.session.user) {
+          return res.send({
+            status: "OK",
+            payload: new UserResponse(req.session.user),
+          });
+        } else {
+          const customeError = new CustomeError({
+            name: "Auth Error",
+            massage: "No fue posible acceder a Current",
+            code: EErrors.AUTHORIZATION_ERROR,
+          });
+          return next(customeError);  
+        }
+      }
+      async uploadFiles (req, res){
+        try{
+          const userId = req.params.uid;
+          const files = req.files;
+          const userUpdate = {};
+          if(files.profiles){
+            userUpdate.profileImage = files.profiles[0].path;
+          }
+          if(files.products){
+            userUpdate.productImage = files.products[0].path;
+          }
+    
+          if(files.document){
+            userUpdate.documents = files.document.map((doc) => ({
+              name: doc.originalname,
+              reference: doc.path,
+              status: "Uploaded",
+            }));
+          }
+          await userModel.findByIdAndUpdate(userId, userUpdate);
+          res.status(200).send("Subido con exito")
+        } catch(error) {
+          res.status(500).send(error.message)
+        }
+       
+      }
+      async updateUserDocuments(req, res){
+        try {
+          const userId = req.params.uid;
+          const file = req.file;
+          if(!file){
+            return res.status(400).send("No se subio nada")
+          }
+          const document = {
+            name: file.originalname,
+            string: file.path,
+          }
+          await userModel.findByIdAndUpdate(userId, {
+            $push:{documents:document},
+            $set:{last_connection: new Date()},
+          });
+          res.status(200).send("Documentos subidos con exito")
+        } catch (error) {
+          res.status(500).send(error.message)
+        }
+      }
+      
+      async upgradeToPremium (req, res){
+        try {
+          const userId = req.params.uid;
+          const user = await userModel.findById(userId);
+          if(!user){
+            return res.status(404).send("Usuario no encontrado")
+          }
+          const requieredDocs = [
+            "identificationDocument",
+            "domicileProofDocument",
+            "accountStatementDocument",
+          ];
+          const hasAllDocuments = requieredDocs.every((docName)=> user.documents.some(
+            (doc) => doc.name === docName && doc.status === "Uploaded"
+          ))
+          if(hasAllDocuments){
+            user.isPremium = true;
+            user.role = "premium";
+            await user.save();
+            res.status(200).send("Actualizado a Premium")
+          } else {
+            res.status(400).send("Los documentos requeridos estan incompletos")
+          }
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error interno")
+        }
+      }
+    
+      async uploadPremiumDocuments(req, res){
+        try {
+          const userId = req.params.uid;
+          const files = req.files;
+          const user = await userModel.findById(userId);
+          if(!user){
+            return res.status(404).send("Usuario no encontrado")
+          }
+          const updateOrAddDocs = (docName, file) => {
+            const existingDocIndex = user.documents.findIndex((doc)=>doc.name === docName);
+            const documentData = {
+              name: docName,
+              reference: file.path,
+              status: "Uploaded",
+            }
+            if(existingDocIndex >= 0){
+              user.documents[existingDocIndex]=documentData;
+            } else {
+              user.documents.push(documentData);
+            }
+          }
+          if(files.identificationDocument){
+            updateOrAddDocs("identificationDocument", files.identificationDocument[0]);
+          }
+          if(files.domicileProofDocument){
+            updateOrAddDocs("domicileProofDocument", files.domicileProofDocument[0]);
+          }
+          if(files.accountStatementDocument){
+            updateOrAddDocs("accountStatementDocument", files.accountStatementDocument[0]);
+          }
+          await user.save();
+          res.status(200).send("Documentacion premium cargada correctamente");
+    
+        } catch (error) {
+          console.error(error);
+          res.status(500).send("Error interno");
+        }
+      }
+    }
+    
+export default UserController;
---
+++ src/dao/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  age: Number,
+  password: String,
+  cart: {type: mongoose.Schema.Types.ObjectId, ref:"carts"},
+  role: String,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  documents: [{
+    name:{
+      type: String,
+      unique: true,
+    },
+    reference: String,
+    status: {
+      type: String,
+      default: "Pending",
+    },
+  }],
+  last_connection: Date,
+});
+
+export const userModel = mongoose.model("users", userSchema);
+export default userModel;
